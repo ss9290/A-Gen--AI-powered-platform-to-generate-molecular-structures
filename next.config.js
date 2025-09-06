728056b78df817c118619c0b1c85dd35f1e72a5b
SHAIK ISMIYA <121305678+ss9290@users.noreply.github.com>
2025-09-06 15:44:59 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,42 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config, { isServer }) {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/@rdkit/rdkit/dist/RDKit_minimal.wasm",
+            to: "static/chunks",
+          },
+        ],
+      }),
+  
+    );
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+  async headers() {
+    return [
+        {
+            // matching all API routes
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
+};
+
+module.exports = nextConfig;
